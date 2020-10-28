c98f6c0cda047834c6fe3fe7b7f01ee2b349c861
hackinthebox <shawn@gold.homelab.infraredhat.com>
2020-10-28 17:33:25 -0400

    auto"

--- packages/NoMachine.sh
+++ packages/NoMachine.sh
@@ -1,8 +1,10 @@
 # download and install 'NoMachine"
+nomachi() {
 echo "Installing NoMachine ..."
-DOWNLO="$HOME/Downloads"
 mkdir -p "$DOWNLO"
 cd "$DOWNLO"
 wget -nv https://download.nomachine.com/download/6.11/Linux/nomachine_6.11.2_1_amd64.deb
-sudo apt-get -y install "${DOWNLO}/nomachine_6.11.2_1_amd64.deb"
+sudo apt-get -y install ./nomachine_6.11.2_1_amd64.deb
 sudo ufw allow 4000
+}
+nomachi | tee -a "$LOGFIL"
